011512616cc73356fc94c07946893c7836ccd065
utkarsh <utkarshrai003@gmail.com>
2017-04-04 15:52:10 +0530

    Adds methods to validate the schema keys and values

--- lib/password_schema_validator/schema_helper_methods.rb
+++ lib/password_schema_validator/schema_helper_methods.rb
@@ -12,6 +12,29 @@ module SchemaHelperMethods
     dictionary: :boolean
   }
 
+  def validate_schema
+    @attributes.each do |attribute|
+      validate_attribute(attribute)
+    end
+  end
+
+  def validate_attribute(attribute)
+    key, value = attribute
+    if RESERVED_OPTIONS[key.to_sym].nil?
+      raise ArgumentError, invalid_key_error(key)
+    elsif !send(RESERVED_OPTIONS[key.to_sym]).include?(value.class)
+      raise ArgumentError, invalid_value_type(key, RESERVED_OPTIONS[key.to_sym])
+    end
+  end
+
+  def invalid_key_error(key)
+    "Invalid key - '"key.to_s + "' passed to the schema. Provide a valid permitted key."
+  end
+
+  def invalid_value_type(key, expected_type)
+    "'" + key.to_s + "' must be of type " + expected_type[0].to_s
+  end
+
   def integer
     [Fixnum]
   end
