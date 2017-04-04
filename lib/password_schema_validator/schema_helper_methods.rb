8e88d07e3ce92157415b5063f3bbb1d472f1949e
utkarsh <utkarshrai003@gmail.com>
2017-04-04 15:43:17 +0530

    Adds reserved options for the validator

--- lib/password_schema_validator/schema_helper_methods.rb
+++ lib/password_schema_validator/schema_helper_methods.rb
@@ -1,2 +1,15 @@
 module SchemaHelperMethods
+
+  RESERVED_OPTIONS = {
+    min_len: :integer,
+    max_len: :integer,
+    lower_case: :boolean,
+    upper_case: :boolean,
+    digits: :integer,
+    special_charecters: :boolean,
+    allowed_special_chareters: :array,
+    discard_words: :array,
+    dictionary: :boolean
+  }
+
 end
