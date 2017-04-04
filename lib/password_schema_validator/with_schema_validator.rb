a79a42e109a88795c3e84df01188855bbcce04ed
utkarsh <utkarshrai003@gmail.com>
2017-04-04 15:38:53 +0530

    Adds initialize method to perform schema check on attributes

--- lib/password_schema_validator/with_schema_validator.rb
+++ lib/password_schema_validator/with_schema_validator.rb
@@ -5,6 +5,15 @@ module ActiveModel
     class WithSchemaValidator < EachValidator
       include SchemaHelperMethods
 
+      def initialize(options)
+        @attributes = filter_options(options)
+        validate_schema
+      end
+
+      def filter_options
+        options.except(:attributes, :class)
+      end
+
     end
   end
 end
