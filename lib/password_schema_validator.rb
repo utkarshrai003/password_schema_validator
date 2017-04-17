2091e3422f91a640427c5d8188a552a563eb013e
utkarsh <utkarshrai003@gmail.com>
2017-04-18 01:17:02 +0530

    Requires active_record

--- lib/password_schema_validator.rb
+++ lib/password_schema_validator.rb
@@ -1,4 +1,5 @@
 
+require 'active_record'
 require 'password_schema_validator/with_schema_validator'
 require 'password_schema_validator/schema_helper_methods'
 require 'password_schema_validator/class_specifier'
